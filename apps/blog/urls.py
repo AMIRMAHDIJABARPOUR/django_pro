2a5bf9663c31f907f6d4168fd58cf33210b45a9f
AMIRMAHDIJABARPOUR <95540461+AMIRMAHDIJABARPOUR@users.noreply.github.com>
2025-11-29 17:36:31 +0330

    first class base api view

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import CreateBlogView , post_list , post_info
+from .views import post_list, post_info
+
 app_name = "blog"
 
 urlpatterns = [
-    path("create/", CreateBlogView.as_view(), name="create"),
-    path('post/',post_list,name="post-list"),
-    path('post/<int:pk>/',post_info,name='post-info')
-    
+    # path("create/", CreateBlogView.as_view(), name="create"),
+    path("post/", post_list.as_view(), name="post-list"),
+    path("post/<int:pk>/", post_info.as_view(), name="post-info"),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,43 +1,56 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_201_CREATED,
+    HTTP_204_NO_CONTENT,
+    HTTP_400_BAD_REQUEST,
+)
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view , permission_classes
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Post
 from .serializers import PostSerializer
+
 # Create your views here.
-class CreateBlogView(APIView):
-    pass
 
-@api_view(['GET','POST'])
-def post_list(request):
-    if request.method=="GET":
-        posts=Post.objects.all()
-        serializer=PostSerializer(posts, many=True)
-        return Response(serializer.data,status=HTTP_200_OK)
-    if request.method=="POST":
-        serializer=PostSerializer(data=request.data)
+
+class post_list(APIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        post = Post.objects.all()
+        serializer = self.serializer_class(post, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
-            post=serializer.save()
-            return Response(serializer.data,status=HTTP_201_CREATED)
-        return Response(serializer.error,status=HTTP_400_BAD_REQUEST)
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
-@api_view(['GET','PUT','DELETE'])
-@permission_classes([IsAuthenticated])
-def post_info(request,pk):
-    if request.method=="GET":
-        posts=get_object_or_404(Post,pk=pk)
-        serializer=PostSerializer(posts)
-        return Response(serializer.data,status=HTTP_200_OK)
-    elif request.method=="PUT":
-        post=get_object_or_404(Post,pk=pk)
-        serialize=PostSerializer(post,request.data)
-        if serialize.is_valid():
-            serialize.save()
-            return Response(serialize.data,status=HTTP_200_OK)
-        return Response(serialize.error_messages,status=HTTP_400_BAD_REQUEST)
-    elif request.method=="DELETE":
-        post=get_object_or_404(Post,pk=pk)
+
+class post_info(APIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        serializer = self.serializer_class(post)
+        return Response(serializer.data, status=HTTP_200_OK)
+
+    def put(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        serializer = self.serializer_class(post, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=HTTP_201_CREATED)
+
+    def delete(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
         post.delete()
-        return Response({"detail": f"Post '{post.title}' has been deleted successfully."},status=HTTP_204_NO_CONTENT)
+        return Response(status=HTTP_204_NO_CONTENT)
+
+
