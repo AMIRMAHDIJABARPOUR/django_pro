5f2f6630eee7a54beda8e0c61532153c86bb807c
AMIRMAHDIJABARPOUR <95540461+AMIRMAHDIJABARPOUR@users.noreply.github.com>
2025-11-30 03:55:13 +0330

    modelviewsett+router

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
-from .views import post_list, post_info
+from django.urls import path , include
 
+from rest_framework.routers import DefaultRouter
+
+from .views import PostModelViewSet
+
+
+router=DefaultRouter()
+router.register('posts',PostModelViewSet,basename='posts')
 app_name = "blog"
 
+
 urlpatterns = [
-    # path("create/", CreateBlogView.as_view(), name="create"),
-    path("post/", post_list.as_view(), name="post-list"),
-    path("post/<int:pk>/", post_info.as_view(), name="post-info"),
+    path('', include(router.urls))
 ]
